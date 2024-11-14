281169556dd7bceae5d73125a380de147de12712
Hari Murugan Ravindran <harimurugan@Hari-MBP.ht.home>
2024-11-14 13:15:43 -0600

    employee-form-entity component and store

---
+++ src/app/components/employee-form-entity/employee-form-entity/employee-form-entity.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-form-entity',
+  templateUrl: './employee-form-entity.component.html',
+  styleUrls: ['./employee-form-entity.component.css']
+})
+export class EmployeeFormEntityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
